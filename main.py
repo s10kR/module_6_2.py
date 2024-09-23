6cd92cb5251624223e944c0c5cb09b179973b422
s10kR <rezrusdar@mail.ru>
2024-09-23 13:57:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 class Vehicle:
     __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
-    def __init__(self, owner, __model, __engine_power, color):
+    def __init__(self, owner, model, engine_power, color):
         self.owner = owner
-        self.__model = __model
-        self.__engine_power = __engine_power
+        self.__model = model
+        self.__engine_power = engine_power
         self.color = color
 
     def get_model(self):
-        print(f'Модель: {self.__model}')
+        return (f'Модель: {self.__model}')
 
     def get_horsepower(self):
-        print(f'Мощность двигателя: {self.__engine_power}')
+        return (f'Мощность двигателя: {self.__engine_power}')
 
     def get_color(self):
-        print(f'Цвет: {self.color}')
+        return (f'Цвет: {self.color}')
 
     def print_info(self):
-        Vehicle.get_model(self)
-        Vehicle.get_horsepower(self)
-        Vehicle.get_color(self)
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
         print(f'Владелец: {self.owner}')
 
     def set_color(self, new_color):
